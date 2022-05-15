603a0138342f2e1b31f0423a0f1a63d6eeba7822
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2022-05-15 12:20:03 +0530

    Initial Commit

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>
+        Made with 🧡 by <b>Devraj Jhala</b>
+      </p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
